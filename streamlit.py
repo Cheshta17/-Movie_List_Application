cc6aa867ee433e029b57d89cabf30cb632916d1f
Cheshta17 <122281116+Cheshta17@users.noreply.github.com>
2024-06-20 11:17:15 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,48 +1,28 @@
 import streamlit as st
+import pickle
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from mlxtend.preprocessing import TransactionEncoder
 
-@st.cache_data
-def load_data():
-    data = pd.read_csv('movie_metadata.csv')
-    return data
+def recommend(movie):
+    movie_idx = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_idx]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])[1:6]
+    
+    recommended_movies = []
+    for i in movie_list:
+        # movie_id = i[0]
+        recommended_movies.append(movies.iloc[i[0]].title)
+    return recommended_movies
 
-def preprocess_data(data):
-    data.dropna(inplace=True)
-    data['title_year'] = data['title_year'].astype(int)
-    x = data['genres'].str.split('|')
-    te = TransactionEncoder()
-    x = te.fit_transform(x)
-    x = pd.DataFrame(x, columns=te.columns_)
-    genres = x.astype('int')
-    genres.insert(0, 'movie_title', data['movie_title'])
-    genres = genres.set_index('movie_title')
-    return genres
+movies_list = pickle.load(open('movies_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_list)
+similarity = pickle.load(open('similarity.pkl','rb'))
 
-def recommendation_movie(movie, x):
-    movie = x[movie + '\xa0']
-    similar_movies = x.corrwith(movie)
-    similar_movies = similar_movies.sort_values(ascending=False)
-    similar_movies = similar_movies.iloc[1:]
-    return similar_movies.head(20)
 
-def main():
-    data = load_data()
+st.title('Movie Recommender System')
 
-    genres = preprocess_data(data)
+selected_movie_name = st.selectbox('Enter a movie name', movies['title'].values)
 
-    st.title("Movie Recommendation System")
-
-    movie_list = list(data['movie_title'].sort_values().values)
-    selected_movie = st.selectbox("Select a movie:", movie_list)
-
-    if st.button("Recommend"):
-        recommendations = recommendation_movie(selected_movie, genres.transpose())
-        st.subheader(f"Similar movies to '{selected_movie}':")
-        st.table(recommendations)
-
-if __name__ == "__main__":
-    main()
+if st.button('Recommend'):
+    recommendations = recommend(selected_movie_name)
+    for i in recommendations:
+        st.write(i)
